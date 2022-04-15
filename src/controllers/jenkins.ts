08a1578ba4d9f3cbc83ead6228b3c859faf6b88d
Sayed Azhar Sabri <afzal.sayed26@gmail.com>
2022-04-16 01:28:22 +0200

    removed space

--- src/controllers/jenkins.ts
+++ src/controllers/jenkins.ts
@@ -1,7 +1,6 @@
 import { RequestHandler } from "express"
 
 export const getSuccess: RequestHandler = (req, res) => {
-    
     res.status(200).json({ status: "success", message: "working fine" });
 }
 
