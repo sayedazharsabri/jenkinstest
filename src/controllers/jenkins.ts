9cfca6b87220be91cda4af2b5fc40f533acc216b
Sayed Azhar Sabri <afzal.sayed26@gmail.com>
2022-04-15 23:07:08 +0200

    Just test

--- src/controllers/jenkins.ts
+++ src/controllers/jenkins.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express"
 
 export const getSuccess: RequestHandler = (req, res) => {
+    
     res.status(200).json({ status: "success", message: "working fine" });
 }
 
